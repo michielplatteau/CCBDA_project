ec6174f47b533196483e03772e558b1cf0903951
michielplatteau <michiel.platteau@outlook.com>
2023-05-11 13:43:19 +0200

    connected to postgres working

--- war_info_app/models.py
+++ war_info_app/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 
 # Create your models here.
-class TestModel(models.Model):
-    model_field = models.CharField(max_length=30)
+# class TestModel(models.Model):
+#     model_field = models.CharField(max_length=30)
+#     date = models.DateTimeField(default=datetime.now)
+
+
+# class TestModel2(models.Model):
+#     count = models.IntegerField()
+#     name = models.CharField(max_length=30)
+
+
+class TestModel3(models.Model):
+    name = models.CharField(max_length=30)
     date = models.DateTimeField(default=datetime.now)
+    tanks = models.IntegerField()
+    fuel = models.IntegerField()
--- war_info_app/views.py
+++ war_info_app/views.py
@@ -3,7 +3,7 @@ import os
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from war_info_app.models import TestModel
+from war_info_app.models import TestModel3
 
 
 # DEFINE OUR VIEWS HERE
@@ -12,13 +12,18 @@ def home(request):
 
 
 def test_model_view(request):
-    tm = TestModel(model_field='Model field value')
+
+    # Add some data for testing
+    tm = TestModel3(name='test_name', tanks=10, fuel=1)
     tm.save()
-    response = TestModel.objects.all()
-    a = ""
+    tm1 = TestModel3(name='test_name1', tanks=20, fuel=3)
+    tm1.save()
+    tm2 = TestModel3(name='test_name2', tanks=30, fuel=2)
+    tm2.save()
+
+    response = TestModel3.objects.all()
+    a = f"{'Name':->20}{'Date':->50}{'Tanks':->10}{'Fuel':->10} <br>"
     for x in response:
-        a += x.model_field
-        a += '; '
-        a += str(x.date)
-        a += '<br>'
+        a += f"{x.name:->20}{str(x.date):->50}{str(x.tanks):->10}{str(x.fuel):->10} <br>"
+    print(a)
     return HttpResponse(a)
