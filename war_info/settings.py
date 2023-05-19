33e11271af9e81819f8d59e4ff9da1aa179b641b
Martin <martinpazicky10@gmail.com>
2023-05-19 14:19:30 +0200

    deployable static files

--- war_info/settings.py
+++ war_info/settings.py
@@ -15,18 +15,23 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%=h+_hf2#1e96j-szb03n_jec=80^#d!*u-52iu#)qj6gtf_v8'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+def str_to_bool(s):
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
 
+
+DEBUG = str_to_bool(os.environ['DJANGO_DEBUG']) if 'DJANGO_DEBUG' in os.environ.keys() else True
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -42,10 +47,7 @@ INSTALLED_APPS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -99,7 +101,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'CCBDA-warinfo',
             'USER': 'postgres',
-            'PASSWORD': '012345',
+            'PASSWORD': '1234',
             'HOST': 'localhost',
             'PORT': '5432'
         }
@@ -131,7 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -143,11 +144,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
