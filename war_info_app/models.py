3da91119b5d43f7084bed33bb491007298b18737
michielplatteau <michiel.platteau@outlook.com>
2023-05-25 12:12:15 +0200

    new models for cleaned data

--- war_info_app/models.py
+++ war_info_app/models.py
@@ -15,11 +15,11 @@ from django.utils import timezone
 #     name = models.CharField(max_length=30)
 
 
-class TestModel3(models.Model):
-    name = models.CharField(max_length=30)
-    date = models.DateTimeField(default=timezone.now)
-    tanks = models.IntegerField()
-    fuel = models.IntegerField()
+# class TestModel3(models.Model):
+#     name = models.CharField(max_length=30)
+#     date = models.DateTimeField(default=timezone.now)
+#     tanks = models.IntegerField()
+#     fuel = models.IntegerField()
 
 
 class Kills(models.Model):
@@ -37,3 +37,41 @@ class EventsMap(models.Model):
     longitude = models.FloatField()
     notes = models.TextField()
     fatalities = models.IntegerField()
+
+
+class Equipment(models.Model):
+    date = models.DateField()
+    day = models.IntegerField()
+    aircraft = models.IntegerField()
+    helicopter = models.IntegerField()
+    tank = models.IntegerField()
+    APC = models.IntegerField()
+    field_artillery = models.IntegerField()
+    MRL = models.IntegerField()
+    military_auto = models.IntegerField()
+    fuel_tank = models.IntegerField()
+    drone = models.IntegerField()
+    naval_ship = models.IntegerField()
+    anti_aircraft_warfare = models.IntegerField()
+    special_equipment = models.IntegerField()
+    mobile_SRBM_system = models.IntegerField()
+    greatest_losses_direction = models.IntegerField()
+    vehicles_and_fuel_tanks = models.IntegerField()
+    cruise_missiles = models.IntegerField()
+    Total_Air_Units = models.IntegerField()
+    Total_Naval_Units = models.IntegerField()
+    Total_Ground_Units = models.IntegerField()
+
+
+class EquipmentPrediction(models.Model):
+    date = models.DateField()
+    aircraft = models.IntegerField()
+    aircraft_lower = models.IntegerField()
+    aircraft_upper = models.IntegerField()
+
+
+class EventsMap2(models.Model):
+    date = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    type = models.TextField()
